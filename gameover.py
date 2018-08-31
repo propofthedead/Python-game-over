988f7fc4a67d295285d4240ac4fb0d61b887f2e3
Foster Stule <camodude5@yahoo.com>
2018-08-31 13:19:50 -0400

    made a bigger text of game over

--- gameover.py
+++ gameover.py
@@ -1,2 +1,31 @@
-print("Game Over")
-input("\n\nPress the enter key to exit")
+print("Program 'Game Over' 2.0")
+
+print("Same", "message", "as before")
+
+print("Just",
+      "a bit",
+      "bigger")
+
+print("Here", end=" ")
+print("it is...")
+
+print(
+        """
+         _____      ___       ___  ___   _______
+        /  ___|    /   |     /   |/   | |   ____| 
+        | |       /    |    / /|   /| | |  |__
+        | |  _   /  /| |   / / |__/ | | |   __|
+        | |_| | /  ___ |  / /       | | |  |____
+        \_____//_/   |_| /_/        |_| |_______|
+        
+         _____    _     _   ______   ______
+        /  _  \  | |   / / | _____| |  _   \
+        | | | |  | |  / /  | |__    | |_|  |
+        | | | |  | | / /   |  __|   |  _   /
+        | |_| |  | |/ /    | |____  | | \  \
+        \_____/  |___/     |______| |_|  \__\
+
+
+        """
+    )
+input("\n\nPress the enter key to exit.")
